26295659dc6527c315ac8b176b89fd33bb22028a
mart6061 <mart6061@gmail.com>
2026-05-16 01:31:38 +0200

    Add country event type registry

--- Shared/Aggregates/Country/CountriesBuilder.cs
+++ Shared/Aggregates/Country/CountriesBuilder.cs
@@ -1,12 +1,16 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using AILibrary.Types;
 
 namespace AILibrary.Aggregates;
 
 public static class CountriesBuilder
 {
+    // TODO: Check for completeness.
+    public static IReadOnlyList<Type> GetCountryEventTypes() =>
+    [
+        typeof(CountryCreatedEvent),
+        typeof(CountryModifiedEvent)
+    ];
+
     public static Countries Create(EventDateTime eventDate, AuditDateTime auditDateTime, List<ICountryEvent> countryEvents)
     {
         if (eventDate is null)
