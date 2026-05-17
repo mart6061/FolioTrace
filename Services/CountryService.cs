6caed98bb6c06522c1417546836427d7b5a566a3
mart6061 <mart6061@gmail.com>
2026-05-17 15:10:24 +0200

    Cache country aggregate reads

--- Services/CountryService.cs
+++ Services/CountryService.cs
@@ -7,13 +7,26 @@ namespace Services;
 
 public sealed class CountryService(IEventRepository eventRepository)
 {
+    private readonly Lock cacheLock = new();
+    private readonly Dictionary<CountryCacheKey, Countries> cache = [];
+
     public async Task<Countries> Get(EventDateTime valuationDate)
     {
         if (valuationDate is null)
             throw new ArgumentNullException(nameof(valuationDate));
 
+        var cacheKey = CountryCacheKey.ForAllAuditHistory(valuationDate);
         var events = await eventRepository.LoadStreamAsync<ICountryEvent>(Constants.Initialisation.CountriesStreamId);
-        return new Countries(valuationDate, events.ToList());
+        var current = new Countries(valuationDate, events.ToList());
+
+        lock (cacheLock)
+        {
+            if (cache.TryGetValue(cacheKey, out var cached) && cached.LastEventID == current.LastEventID)
+                return cached;
+
+            cache[cacheKey] = current;
+            return current;
+        }
     }
 
     public async Task<Countries> Get(EventDateTime valuationDate, AuditDateTime asAt)
@@ -24,7 +37,30 @@ public sealed class CountryService(IEventRepository eventRepository)
         if (asAt is null)
             throw new ArgumentNullException(nameof(asAt));
 
+        var cacheKey = CountryCacheKey.ForAsAt(valuationDate, asAt);
+
+        lock (cacheLock)
+        {
+            if (cache.TryGetValue(cacheKey, out var cached))
+                return cached;
+        }
+
         var events = await eventRepository.LoadStreamAsync<ICountryEvent>(Constants.Initialisation.CountriesStreamId);
-        return new Countries(valuationDate, asAt, events.ToList());
+        var current = new Countries(valuationDate, asAt, events.ToList());
+
+        lock (cacheLock)
+        {
+            cache[cacheKey] = current;
+            return current;
+        }
+    }
+
+    private readonly record struct CountryCacheKey(DateTime ValuationDateTime, DateTime? AsAtDateTime)
+    {
+        public static CountryCacheKey ForAllAuditHistory(EventDateTime valuationDate) =>
+            new(valuationDate.Value, null);
+
+        public static CountryCacheKey ForAsAt(EventDateTime valuationDate, AuditDateTime asAt) =>
+            new(valuationDate.Value, asAt.Value);
     }
 }
