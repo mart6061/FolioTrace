4b0b263283e450a41a01258494e3e9e5cc30f06c
mart6061 <mart6061@gmail.com>
2026-05-17 16:28:51 +0200

    Add yesterday valuation constant

--- Shared/Constants.cs
+++ Shared/Constants.cs
@@ -21,16 +21,19 @@ public static class Constants
         [UIDetails(Short: "Today", Long: "End of today", Order: 1)]
         public static readonly EventDateTime Today = EventDateTimeBuilder.Create(DateTime.Now.AddDays(1).AddTicks(-1));
 
-        [UIDetails(Short: "Month End", Long: "End of the current calendar month", Order: 2)]
+        [UIDetails(Short: "Yesterday", Long: "Yesterday or last working day", Order: 2)]
+        public static readonly EventDateTime Yesterday = EventDateTimeBuilder.Create(DateTime.Now.Date.AddDays(DateTime.Now.DayOfWeek == DayOfWeek.Monday ? -2 : 0).AddTicks(-1));
+
+        [UIDetails(Short: "Month End", Long: "End of the current calendar month", Order: 3)]
         public static readonly EventDateTime MonthEnd = EventDateTimeBuilder.Create(new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1).AddMonths(1).AddTicks(-1));
 
-        [UIDetails(Short: "Last Month End", Long: "End of the previous calendar month", Order: 3)]
+        [UIDetails(Short: "Last Month End", Long: "End of the previous calendar month", Order: 4)]
         public static readonly EventDateTime LastMonthEnd = EventDateTimeBuilder.Create(new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1).AddTicks(-1));
 
-        [UIDetails(Short: "Last Year End", Long: "End of the previous calendar year", Order: 4)]
+        [UIDetails(Short: "Last Year End", Long: "End of the previous calendar year", Order: 5)]
         public static readonly EventDateTime LastYearEnd = EventDateTimeBuilder.Create(new DateTime(DateTime.Now.Year, 1, 1).AddTicks(-1));
 
-        [UIDetails(Short: "Last Financial Year End", Long: "End of the previous financial year ending 31 March", Order: 5)]
+        [UIDetails(Short: "Last Financial Year End", Long: "End of the previous financial year ending 31 March", Order: 6)]
         public static readonly EventDateTime LastFinancialYearEnd = EventDateTimeBuilder.Create(DateTime.Now.Date > new DateTime(DateTime.Now.Year, 3, 31) ? new DateTime(DateTime.Now.Year, 4, 1).AddTicks(-1) : new DateTime(DateTime.Now.Year - 1, 4, 1).AddTicks(-1));
     }
 }
