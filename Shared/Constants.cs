2a0b1fdf5013f6265f7a67bd13240bedc652e1bf
mart6061 <mart6061@gmail.com>
2026-05-17 14:18:58 +0200

    Describe valuation constants

--- Shared/Constants.cs
+++ Shared/Constants.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel;
 using FolioTrace.Types;
 
 namespace FolioTrace;
@@ -17,14 +18,19 @@ public static class Constants
 
     public static class Valuation
     {
+        [Description("End of today")]
         public static readonly EventDateTime Today = EventDateTimeBuilder.Create(DateTime.Now.AddDays(1).AddTicks(-1));
 
+        [Description("End of the current calendar month")]
         public static readonly EventDateTime MonthEnd = EventDateTimeBuilder.Create(new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1).AddMonths(1).AddTicks(-1));
 
+        [Description("End of the previous calendar month")]
         public static readonly EventDateTime LastMonthEnd = EventDateTimeBuilder.Create(new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1).AddTicks(-1));
 
+        [Description("End of the previous calendar year")]
         public static readonly EventDateTime LastYearEnd = EventDateTimeBuilder.Create(new DateTime(DateTime.Now.Year, 1, 1).AddTicks(-1));
 
+        [Description("End of the previous financial year ending 31 March")]
         public static readonly EventDateTime LastFinancialYearEnd = EventDateTimeBuilder.Create(DateTime.Now.Date > new DateTime(DateTime.Now.Year, 3, 31) ? new DateTime(DateTime.Now.Year, 4, 1).AddTicks(-1) : new DateTime(DateTime.Now.Year - 1, 4, 1).AddTicks(-1));
     }
 }
